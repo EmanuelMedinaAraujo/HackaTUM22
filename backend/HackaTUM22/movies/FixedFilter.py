bbe4738ebb1d8acbbbb022a49599016a18947835
Emanuel Medina <emanuel.andre@web.de>
2022-11-19 12:37:20 +0100

    Adapted Fixed Filter

--- backend/HackaTUM22/movies/FixedFilter.py
+++ backend/HackaTUM22/movies/FixedFilter.py
@@ -2,10 +2,18 @@ import models
 
 # Fixed Filter determinded by the user
 class FixedFilter():
+
+    # List of strings of genres
     genres = []
+
+    # Flag for filtering between movies and series
     showOnlyMovie = False
+
+    # List of possible providers
     providers = []
-    price = 0.0
+
+    # Flag for 'inSubscription', 'free' and 'pay or lend'
+    price = (True, False, False)
 
     # Constructor
     def __init__(self, genres, showOnlyMovie, providers, price):
