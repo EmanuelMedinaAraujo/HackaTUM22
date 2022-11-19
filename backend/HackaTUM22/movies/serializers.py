4b7fe13f48c35f9c91fa79b90e03b5a8e28e16ff
Lukas Krimphove <lukas.krimphove@gmx.de>
2022-11-19 20:11:57 +0100

    update

--- backend/HackaTUM22/movies/serializers.py
+++ backend/HackaTUM22/movies/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from .models import *
+from .models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ['tmdb', 'tvdb', 'imdb_id', 'imdb_episode_id', 'title', 'otitle', 'original', 'serie', 'episode',
-                  'episodetitle', 'year', 'directors', 'actors', 'companies', 'genres', 'countries', 'banner', 'poster',
-                  'runtime', 'fsk']
+                  'episodetitle', 'year', 'directors', 'actors', 'companies', 'genres', 'countries', 'banners',
+                  'posters', 'runtime', 'fsk']
